96545728386f40e25e1a1b0cb3dec490b69794ca
htogether7 <htogether7@gmail.com>
2023-08-07 15:32:17 +0900

    FIX : TimeInput FORMAT 추가

--- scheduler/src/components/TimeInput/TimeInput.js
+++ scheduler/src/components/TimeInput/TimeInput.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const TimeInput = ({ id, content }) => {
+const TimeInput = ({ id, content, func }) => {
+  const funcFormat = (e) => {
+    func(e.target.value);
+  };
+
   return (
     <>
       <label for={id}>{content}</label>
-      <input id={id} className="timeInput" type="text" />
+      <input id={id} className="timeInput" type="text" onChange={funcFormat} />
     </>
   );
 };
