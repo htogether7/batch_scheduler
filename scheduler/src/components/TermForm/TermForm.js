b7afa8b46effa1d8aacc00dffbca44a2456e2350
htogether7 <htogether7@gmail.com>
2023-08-07 11:21:29 +0900

    FIX : 이전과 같은 값으로 설정했을 때에는 term을 재설정하지 않도록함.

--- scheduler/src/components/TermForm/TermForm.js
+++ scheduler/src/components/TermForm/TermForm.js
@@ -8,15 +8,21 @@ const TermForm = ({ term, termInput, setTermInput }) => {
   const handleTermSubmit = (e) => {
     e.preventDefault();
     if (validateTerm(termInput)) {
-      term.current = termInput;
-      alert(`다음 작업들은 ${termInput}ms 간격으로 실행됩니다.`);
+      if (term.current === termInput) {
+        alert("이전과 같은 값입니다.");
+      } else {
+        term.current = termInput;
+        alert(`다음 작업들은 ${termInput}ms 간격으로 실행됩니다.`);
+      }
     } else {
       alert("잘못된 입력이거나 너무 짧은 시간입니다.");
     }
   };
+
   const handleChange = (e) => {
     setTermInput(e.target.value);
   };
+
   return (
     <>
       <form onSubmit={handleTermSubmit}>
