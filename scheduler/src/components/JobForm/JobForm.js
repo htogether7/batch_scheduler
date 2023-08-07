48931163414afce824fc8f6b334aa2ce830cb37f
htogether7 <htogether7@gmail.com>
2023-08-07 09:50:07 +0900

    ADD : 작업 등록 폼 추가

---
+++ scheduler/src/components/JobForm/JobForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./JobForm.css";
+import TimeInput from "../TimeInput/TimeInput";
+
+const JobForm = ({ mode }) => {
+  return (
+    <>
+      <form>
+        <div>작업 등록</div>
+        <input type="text" required />
+        <br></br>
+        <button type="button">주기</button>
+        <button type="button">조건</button>
+        <div>
+          <TimeInput id="month" content="월" />
+          <TimeInput id="day" content="일" />
+          <TimeInput id="hour" content="시" />
+          <TimeInput id="minute" content="분" />
+        </div>
+      </form>
+      <button className="resetButton">전체 작업 초기화</button>
+    </>
+  );
+};
+
+export default JobForm;
