96288cb22c10da48815777020239775570d5315a
htogether7 <htogether7@gmail.com>
2023-08-07 09:51:47 +0900

    ADD : 제출 버튼 추가

--- scheduler/src/components/JobForm/JobForm.js
+++ scheduler/src/components/JobForm/JobForm.js
@@ -11,12 +11,13 @@ const JobForm = ({ mode }) => {
         <br></br>
         <button type="button">주기</button>
         <button type="button">조건</button>
-        <div>
+        <>
           <TimeInput id="month" content="월" />
           <TimeInput id="day" content="일" />
           <TimeInput id="hour" content="시" />
           <TimeInput id="minute" content="분" />
-        </div>
+        </>
+        <button type="submit">제출</button>
       </form>
       <button className="resetButton">전체 작업 초기화</button>
     </>
