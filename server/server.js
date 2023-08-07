303965810ab8c0fbfc035c9b684be8902bc08a20
htogether7 <htogether7@gmail.com>
2023-08-07 16:28:10 +0900

    DELETE : 주석 제거

--- server/server.js
+++ server/server.js
@@ -28,27 +28,6 @@ connection.connect((err) => {
   }
 });
 
-// app.get("/api/login", (req, res) => {
-//   const submitted_id = req.query.id;
-//   const submitted_pw = req.query.pw;
-//   const sql = `SELECT * FROM user WHERE user.id = "${submitted_id}" and user.pw = "${submitted_pw}"`;
-//   db.query(sql, (err, result, fields) => {
-//     if (err) throw err;
-//     else {
-//       if (result.length == 0) {
-//         console.log("다시 확인 해주세요");
-//         res.json({ login: 0 });
-//       } else {
-//         console.log("로그인 성공");
-//         res.cookie("user", submitted_id, {
-//           expires: new Date(Date.now() + 900000),
-//         });
-//         res.json({ login: 1 });
-//       }
-//     }
-//   });
-// });
-
 app.get("/job", (req, res) => {
   const sql = `select * from job_info;`;
   connection.query(sql, (err, result) => {
