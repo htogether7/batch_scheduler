16baa2a2b8c5e96f027b7266c8e634b286c191c4
htogether7 <htogether7@gmail.com>
2023-08-07 11:13:06 +0900

    FEAT : term 입력 시 검증 기능

--- scheduler/src/components/TermForm/TermForm.js
+++ scheduler/src/components/TermForm/TermForm.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
 const TermForm = ({ term, termInput, setTermInput }) => {
+  const validateTerm = (termInput) => {
+    return isNaN(termInput) ? false : parseInt(termInput) < 1000 ? false : true;
+  };
+
   const handleTermSubmit = (e) => {
     e.preventDefault();
-    term.current = termInput;
-    alert(`다음 작업들은 ${term.current}ms 간격으로 실행됩니다.`);
+    if (validateTerm(termInput)) {
+      term.current = termInput;
+      alert(`다음 작업들은 ${termInput}ms 간격으로 실행됩니다.`);
+    } else {
+      alert("잘못된 입력이거나 너무 짧은 시간입니다.");
+    }
   };
   const handleChange = (e) => {
     setTermInput(e.target.value);
@@ -13,7 +21,7 @@ const TermForm = ({ term, termInput, setTermInput }) => {
     <>
       <form onSubmit={handleTermSubmit}>
         <div>Term 설정</div>
-        <input type="text" onChange={handleChange} />
+        <input type="text" onChange={handleChange} placeholder="1000ms 이상" />
         <button type="submit">제출</button>
       </form>
     </>
