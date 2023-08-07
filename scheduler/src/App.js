620fdd1e4b14b33bbc8405265ae54f5c4a7e3dcb
htogether7 <htogether7@gmail.com>
2023-08-07 09:54:18 +0900

    ADD : Term 등록 폼 추가

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,28 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import List from "./components/List/List";
+import JobForm from "./components/JobForm/JobForm";
+import TermForm from "./components/TermForm/TermForm";
 
 function App() {
+  const [jobList, setJobList] = useState([1, 2]);
+  const [mode, setMode] = useState("time");
+
+  // 처음 마운트 되었을 때 DB로 부터 JobList를 불러옴
+
   useEffect(() => {
     axios.get("http://localhost:5050/api").then((r) => console.log(r.data));
   });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List jobList={jobList} />
+      <TermForm />
+      <JobForm mode={mode} />
     </div>
   );
 }
---
+++ scheduler/src/components/TermForm/TermForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TermForm = () => {
+  return (
+    <>
+      <form>
+        <div>Term 설정</div>
+        <input type="text" />
+        <button type="submit">제출</button>
+      </form>
+    </>
+  );
+};
+
+export default TermForm;
