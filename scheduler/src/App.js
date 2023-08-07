5c2ab5336111d6ae04dd009cbc9cbf4c2e630ef1
htogether7 <htogether7@gmail.com>
2023-08-07 11:06:35 +0900

    FIX : term을 설정해도, 이전 term을 가진 무한 루프가 계속 실행되는 에러 해결

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import List from "./components/List/List";
 import JobForm from "./components/JobForm/JobForm";
 import TermForm from "./components/TermForm/TermForm";
@@ -8,28 +8,29 @@ import TermForm from "./components/TermForm/TermForm";
 function App() {
   const [submittedJobs, setSubmittedJobs] = useState([1, 2, 3]);
   const [mode, setMode] = useState("time");
-  const [term, setTerm] = useState(1000);
+  const term = useRef(1000);
+  const [termInput, setTermInput] = useState(0);
 
   const infiniteCheck = async (term) => {
+    const roofTerm = term;
     while (true) {
-      await new Promise((r) => setTimeout(r, term)).then(() =>
-        console.log(`now term : ${term}, time : ${Date.now()}`)
+      if (roofTerm !== term) break;
+      await new Promise((r) => setTimeout(r, term.current)).then(() =>
+        console.log(`now term : ${term.current}, time : ${Date.now()}`)
       );
     }
   };
 
   // 처음 마운트 되었을 때 DB로 부터 JobList를 불러옴
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:5050/api").then((r) => console.log(r.data));
-  // });
   useEffect(() => {
     infiniteCheck(term);
-  });
+  }, [term]);
+
   return (
     <div className="App">
       <List jobs={submittedJobs} />
-      <TermForm />
+      <TermForm term={term} termInput={termInput} setTermInput={setTermInput} />
       <JobForm mode={mode} />
     </div>
   );
--- scheduler/src/components/TermForm/TermForm.js
+++ scheduler/src/components/TermForm/TermForm.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TermForm = () => {
+const TermForm = ({ term, termInput, setTermInput }) => {
+  const handleTermSubmit = (e) => {
+    e.preventDefault();
+    term.current = termInput;
+    alert(`다음 작업들은 ${term.current}ms 간격으로 실행됩니다.`);
+  };
+  const handleChange = (e) => {
+    setTermInput(e.target.value);
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleTermSubmit}>
         <div>Term 설정</div>
-        <input type="text" />
+        <input type="text" onChange={handleChange} />
         <button type="submit">제출</button>
       </form>
     </>
