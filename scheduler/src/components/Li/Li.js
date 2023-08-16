a33ee199058377ca9ef022bf266019abb6e43b59
htogether7 <htogether7@gmail.com>
2023-08-16 13:46:34 +0900

    FIX : id 추가

--- scheduler/src/components/Li/Li.js
+++ scheduler/src/components/Li/Li.js
@@ -9,7 +9,6 @@ const Li = ({
   mode,
 }) => {
   const handleLiClick = (e) => {
-    console.log(e.target);
     if (mode === "condition") {
       setSelectedCondition(e.target.id);
     }
@@ -17,20 +16,19 @@ const Li = ({
   return (
     <li onClick={handleLiClick} key={job.enrolled_time} id={job.enrolled_time}>
       <div id={job.enrolled_time}>
-        <div>{job.name}</div>
+        <div id={job.enrolled_time}>{job.name}</div>
         <div id={job.enrolled_time}>
           {job.month
             ? `실행 시간 :${job.month}월 ${job.day}일 ${job.hour}시 ${job.minute}분`
             : ` ${
-                ("선행 조건 :",
                 jobs.filter((obj) => obj.enrolled_time === job.pre_condition)[0]
                   ? jobs.filter(
                       (obj) => obj.enrolled_time === job.pre_condition
                     )[0].name
-                  : "선행 조건이 제거되어 실행 불가")
+                  : "선행 조건이 제거되어 실행 불가"
               }`}
         </div>
-        <div>실행 파일 : ${job.route}</div>
+        <div id={job.enrolled_time}>실행 파일 : ${job.route}</div>
       </div>
       <button onClick={handleUpdateClick} id={job.enrolled_time}>
         수정
