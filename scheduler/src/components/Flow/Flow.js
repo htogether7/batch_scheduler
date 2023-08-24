4a9b49ac897a0e7d165dde3ea36af7111dd1d2db
htogether7 <htogether7@gmail.com>
2023-08-24 13:18:28 +0900

    ADD : 작업 흐름도 컴포넌트 추가

---
+++ scheduler/src/components/Flow/Flow.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Flow.css";
+
+const Flow = () => {
+  return (
+    <div className="flowContainer">
+      <div className="title">작업 흐름</div>
+      <div className="graphContainer">111</div>
+    </div>
+  );
+};
+
+export default Flow;
